077b2e0f1b41b2fb0dc6307740ac736c6e09aa4d
alex-wp <alex.musichen@gmail.com>
2016-07-02 16:35:08 +0200

    init web template index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>web1</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="script.js">
+</head>
+<body>
+	
+</body>
+</html>
